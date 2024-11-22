0efef358e2327b1b3d9b5a511eeb24a8555f5075
Xingyou Liu <liuxingyou666@outlook.com>
2024-11-22 16:30:08 +0000

    Update player.py

--- player.py
+++ player.py
@@ -14,12 +14,11 @@ class Player:
         self.cards.append(new_card)
         self.hand.append(card_tuple)
 
-    def remove_card(self, card_tuple: Tuple[str, int]):
-        self.hand.remove(card_tuple)
-        for card in self.cards:
-            if card.color == card_tuple[0] and card.number == card_tuple[1]:
-                self.cards.remove(card)
-                break
+    def remove_card(self, card: Card) -> Card:
+        card_index = self.cards.index(card)
+        self.hand.pop(card_index)
+        removed_card = self.cards.pop(card_index)
+        return removed_card
 
     def exist_valid_group(self) -> bool:
         collection = CollectionOfCards(self.hand)
