fbcd810af76b3279662e8a892f2917a14d171320
Xingyou Liu <liuxingyou666@outlook.com>
2024-11-22 12:19:27 +0000

    Update player.py

--- player.py
+++ player.py
@@ -1,25 +1,25 @@
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 from collection_of_cards import CollectionOfCards
-import random
+from card import Card
 
 class Player:
-    def __init__(self, name: str, is_human: bool = True) -> None:
+    def __init__(self, name: str, is_human: bool = True):
         self.name = name
-        self.hand: List[Tuple[str, int]] = []
         self.is_human = is_human
+        self.cards: List[Card] = []  
+        self.hand: List[Tuple[str, int]] = []  
 
+    def add_card(self, card_tuple: Tuple[str, int], position: Tuple[int, int] = (0, 0)):
+        new_card = Card(card_tuple[0], card_tuple[1], position)
+        self.cards.append(new_card)
+        self.hand.append(card_tuple)
 
-    def add_card(self, card: Tuple[str, int]) -> None:
-        self.hand.append(card)
-
-
-    def remove_card(self, card: Tuple[str, int]) -> None:
-        self.hand.remove(card)
-
-
-    def show_cards(self) -> str:
-        return ', '.join(card[0] + ' ' + str(card[1]) for card in self.hand)
-    
+    def remove_card(self, card_tuple: Tuple[str, int]):
+        self.hand.remove(card_tuple)
+        for card in self.cards:
+            if card.color == card_tuple[0] and card.number == card_tuple[1]:
+                self.cards.remove(card)
+                break
 
     def exist_valid_group(self) -> bool:
         collection = CollectionOfCards(self.hand)
@@ -39,14 +39,9 @@ class Player:
     def all_valid_groups_with_largest_length(self) -> List[List[Tuple[str, int]]]:
         collection = CollectionOfCards(self.hand)
         return collection.all_valid_groups_with_largest_length()
-    
-    
-    def probability_exist_valid_group_drawing_one_card(self) -> float:
-        collection = CollectionOfCards(self.hand)
-        return collection.probability_exist_valid_group_drawing_one_card()
-    
-    def get_computer_action(self, available_actions: List[str]) -> str:
-        return random.choice(available_actions)
 
-    def should_continue_drawing(self) -> bool:
-        return random.choice([True, False])
+    def clear_selections(self):
+        for card in self.cards:
+            card.selected = False
+            card.hover = False
+            card.invalid = False
